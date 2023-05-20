dd3b15ba9f0ef841bebf2ab9927d851888696080
suvanadhikari <53096723+suvanadhikari@users.noreply.github.com>
2023-05-20 12:43:31 -0400

    Set up the chess board

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Board } from "./board.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/board.js
@@ -0,0 +1,62 @@
+import "./board.css"
+import React from "react";
+import Chessboard from 'chessboardjsx'
+import { Chess } from "chess.js"
+
+
+class Board extends React.Component {
+
+    state = {
+        "fen": "start",
+        "humanTurn": "w"
+    }
+
+    board = new Chess()
+
+
+    handleHumanMove(move) {
+        if (this.board.turn() !== this.state.humanTurn || this.board.isGameOver()) {
+            return
+        }
+        if (move.to[1] == "1" || move.to[1] == "8") {
+            let piece = this.board.get(move.from).type
+            if (piece == "p") {
+                move.promotion = prompt("What would you like to promote to (q, r, b, n)?")
+            }
+        }
+        try {
+            this.board.move(move)
+            this.setState({"fen": this.board.fen()})
+            if (this.board.isGameOver()) {
+                return
+            }
+            setTimeout(() => {
+                this.board.move(this.getComputerMove())
+                this.setState({"fen": this.board.fen()})
+            }, 300)
+        }
+        catch(err) {
+            return
+        }
+    }
+
+    getComputerMove() {
+        let moves = this.board.moves()
+        return moves[Math.floor(Math.random() * moves.length)]
+    }
+
+    render() {
+        return (
+            <div id="chessboard">
+                <Chessboard position={this.state.fen} onDrop={(move) => {
+                    this.handleHumanMove({
+                        from: move.sourceSquare,
+                        to: move.targetSquare,
+                    })
+                }}></Chessboard>
+            </div>
+        );
+    }
+}
+
+export { Board }
