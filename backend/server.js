088d1219ebee5afebfe50de788021c96a246de57
suvanadhikari <53096723+suvanadhikari@users.noreply.github.com>
2023-05-25 01:33:26 -0400

    Stockfish backend added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,20 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const fs = require("fs")
+const Engine = require("node-uci").Engine
+
+wasmPath = require("path").join(__dirname, "node_modules", "stockfish", "src", "stockfish.wasm");
+mod = {
+    locateFile: function (path){
+        if (path.indexOf(".wasm") > -1) {
+            /// Set the path to the wasm binary.
+            return wasmPath;
+        } else {
+            /// Set path to worker (self + the worker hash)
+            return __filename;
+        }
+    },
+};
 
 // Temporary - will be put in a postgres database soon
 const puzzles = fs.readFileSync("../positions/random_evals.csv", "utf-8").split("\n")
@@ -10,10 +24,11 @@ const numPuzzles = puzzles.length
 const PORT = 4000
 const app = express()
 
-const puzzleEndpoint = express.Router()
-
 app.use(cors())
 app.use(bodyParser.json())
+
+
+const puzzleEndpoint = express.Router()
 
 puzzleEndpoint.route("/").get((req, res) => {
     // Temporary - will be put in a postgres database soon
@@ -23,8 +38,29 @@ puzzleEndpoint.route("/").get((req, res) => {
     })
 })
 
-
 app.use('/getpuzzle', puzzleEndpoint)
+
+
+// Eval stuff
+const engine = new Engine('./stockfish/stockfish-windows-2022-x86-64-avx2.exe')
+
+
+const evaluationEndpoint = express.Router()
+
+evaluationEndpoint.route("/").get((req, res) => {
+    let fen = req.body.fen
+    engine.chain()
+        .init()
+        .setoption('MultiPV', 3)
+        .position(fen)
+        .go({depth: 10})
+        .then(result => {
+            res.send(JSON.stringify(result))
+        })
+})
+
+app.use('/evaluate', evaluationEndpoint)
+
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
