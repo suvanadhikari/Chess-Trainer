046968aa77645678b5a5705a89da7bf0248c8548
suvanadhikari <53096723+suvanadhikari@users.noreply.github.com>
2023-07-22 16:42:18 -0500

    Connected settings to state

--- frontend/src/settingsOverlay.js
+++ frontend/src/settingsOverlay.js
@@ -4,6 +4,10 @@ import ReactModal from "react-modal"
 class SettingsOverlay extends React.Component {
     state = {
         showOverlay: false,
+        limitStrength: false,
+        engineElo: 3000,
+        moveDepth: 15,
+        reviewDepth: 20
     }
 
     openOverlay() {
@@ -16,8 +20,20 @@ class SettingsOverlay extends React.Component {
 
     render() {
         const modalStyle = {
+            content: {
+                width: "35vw",
+                height: "35vh",
+                backgroundColor: "#232323",
+                borderColor: "#232323",
+                color: "white",
+                left: "50%",
+                right: "auto",
+                top: "50%",
+                transform: "translate(-50%, -50%)"
+            },
             overlay: {
-                zIndex: 999
+                zIndex: 999,
+                backgroundColor: "rgba(0, 0, 0, 0.8)"
             }
         }
         return (
@@ -26,7 +42,20 @@ class SettingsOverlay extends React.Component {
                     isOpen={this.state.showOverlay} 
                     onRequestClose={this.closeOverlay.bind(this)}
                     style={modalStyle}>
-                    <p>Settings go here!</p>
+                    <h3>Settings:</h3>
+                    Limit engine strength (for opponent's moves): <input type="checkbox" onChange={(e)=>{this.setState({limitStrength: e.target.checked})}}></input>
+                    <br></br>
+                    {
+                        this.state.limitStrength &&
+                        <>
+                            Engine strength (for opponent's moves): <input type="range" min="1000" max="3000" defaultValue="3000" onChange={(e)=>{this.setState({engineElo: e.target.value})}}></input>
+                            <br></br>
+                        </>
+                    }
+                    <br></br>
+                    Engine depth (for opponent's moves): <input type="range" min="10" max="20" defaultValue="15" onChange={(e)=>{this.setState({moveDepth: e.target.value})}}></input>
+                    <br></br>
+                    Engine depth (for review): <input type="range" min="17" max="23" defaultValue="20" onChange={(e)=>{this.setState({reviewDepth: e.target.value})}}></input>
                 </ReactModal>
                 <button onClick={this.openOverlay.bind(this)}>Settings</button>
             </>
